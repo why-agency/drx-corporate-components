0c343e626fef0f8aaf79897da4b01be2445a6f1a
Jan Demmerle <jan.demmerle@why.de>
2022-05-17 15:14:25 +0200

    chore: bundle components into custom elements when building package

---
+++ lib/main.ts
@@ -0,0 +1,7 @@
+import {defineCustomElement} from 'vue'
+
+import O0204IntroHero from '../src/components/organisms/o-02-04-IntroHero.ce.vue'
+
+const O0204IntroHeroElement = defineCustomElement(O0204IntroHero)
+
+export { O0204IntroHeroElement }
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import O0204IntroHero from './components/organisms/o-02-04-IntroHero.vue'
+import O0204IntroHero from './components/organisms/o-02-04-IntroHero.ce.vue'
 </script>
 
 <template>
