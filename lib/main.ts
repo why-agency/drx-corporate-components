b4be238ba0b7236f7c2679e9f3b49004f410b6d1
Jan Demmerle <jan.demmerle@why.de>
2022-05-17 15:47:26 +0200

    feat: export register function

--- lib/main.ts
+++ lib/main.ts
@@ -5,3 +5,7 @@ import O0204IntroHero from '../src/components/organisms/o-02-04-IntroHero.ce.vue
 const O0204IntroHeroElement = defineCustomElement(O0204IntroHero)
 
 export { O0204IntroHeroElement }
+
+export function register() {
+  customElements.define('o-02-04-intro-hero', O0204IntroHeroElement)
+}
