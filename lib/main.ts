515247b7fb64b34a915156b66732fd9957287647
Alexandra Panas <alexandra.panas@why.de>
2022-05-19 16:11:07 +0200

    feat: disable shadow dom & remove tw plugin

--- lib/main.ts
+++ lib/main.ts
@@ -1,4 +1,4 @@
-import {defineCustomElement} from 'vue'
+import { defineCustomElement } from './localApiCustomElement'
 
 import O0204IntroHero from '../src/components/organisms/o-02-04-IntroHero.ce.vue'
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-import vueTwind from 'vite-plugin-vue-twind'
 
 import * as path from 'path'
 
 module.exports = defineConfig({
-  plugins: [vue(), vueTwind()],
+  plugins: [vue()],
   build: {
     lib: {
-      entry: path.resolve(
-        __dirname,
-        'lib/main.ts'
-      ),
+      entry: path.resolve(__dirname, 'lib/main.ts'),
       name: 'VueDrx',
       fileName: format => `drx-corporate-components.${format}.js`
     }
