93a89178e94c79cd41f34c1b2108f86957eead9c
Alexandra Panas <alexandra.panas@why.de>
2022-05-19 17:51:07 +0200

    feat: add base components and composables and render them on a page

---
+++ src/components/base/Headline.vue
@@ -0,0 +1,42 @@
+<template>
+  <component
+    :is="headlineTag"
+    class="font-semibold font-primary min-h-safari"
+    :class="$_headlineStyle"
+  >
+    <slot>
+      <BaseHtmlParser tag="span" :content="text" />
+    </slot>
+  </component>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import BaseHtmlParser from './HtmlParser.vue'
+
+const props = defineProps({
+  text: {
+    type: String,
+    default: ''
+  },
+  tag: {
+    type: String,
+    default: 'div'
+  },
+  layout: {
+    type: Number,
+    default: 1
+  }
+})
+
+const headlineTag = computed(() => props.tag || `h${props.layout}`)
+
+const $_headlineStyle = computed(() => ({
+  'text-h1': props.layout === 1,
+  'text-h2': props.layout === 2,
+  'text-h3': props.layout === 3,
+  'text-h4': props.layout === 4,
+  'text-h5': props.layout === 5,
+  'text-h6': props.layout === 6
+}))
+</script>
--- src/components/organisms/o-02-04-IntroHero.ce.vue
+++ src/components/organisms/o-02-04-IntroHero.ce.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>
-    <h1 class="text-red-500">This is a test {{ headline }}</h1>
+  <div class="mb-4">
+    <BaseHeadline v-bind="headlineProps" />
+    <BaseSubline text="This is a subline" :layout="2" />
+    <BaseAction to="https://draexlmaier.loc/"
+      >Action
+      <template v-slot:iconSuffix>
+        <ArrowDownFat />
+      </template>
+      <template v-slot:iconPrefix>
+        <ArrowLeftFat />
+      </template>
+    </BaseAction>
   </div>
 </template>
+<script setup>
+import { ref } from "vue";
+import BaseHeadline from "../base/Headline.vue";
+import BaseSubline from "../base/Subline.vue";
+import BaseAction from "../base/Action.vue";
+import ArrowDownFat from "../icons/Arrow/DownFat.vue";
+import ArrowLeftFat from "../icons/Arrow/LeftFat.vue";
 
-<script setup lang="ts">
 const props = defineProps({
-  headline: String
-})
+  data: {
+    type: Object,
+    default: () => {},
+  },
+});
+
+const headlineProps = ref({ text: "This is a headline", layout: 3, tag: "h2" });
 </script>
