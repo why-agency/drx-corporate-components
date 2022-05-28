cbb13b3aa24079de778082612121fe3ea0cebc14
Alexandra Panas <alexandra.panas@why.de>
2022-05-28 17:58:54 +0200

    feat: install and use vue router

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
+import * as VueRouter from 'vue-router'
 import App from './App.vue'
+import index from './pages/index.vue'
+import fullscreenMediaText from './pages/fullscreen-media-text.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+const routes = [
+    { path: '/', component: index},
+    { path: '/fullscreen-media-text', component: fullscreenMediaText }
+  ]
+
+const router = VueRouter.createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: VueRouter.createWebHashHistory(),
+    routes, // short for `routes: routes`
+  })
+
+  app.use(router)
+  app.mount('#app')
