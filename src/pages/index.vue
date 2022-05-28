f9a608e0fb70db357eaa4a94ce038ea15422fef8
Alexandra Panas <alexandra.panas@why.de>
2022-05-28 18:00:39 +0200

    feat: create pages for each component

---
+++ src/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-primary flex flex-col space-y-10 ml-10 mt-20">
+    <router-link to="/fullscreen-media-text">Fullscreen Media Text</router-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
