63c2e1c9d2e5701be98763cc33c9cb55d1909625
Alexandra Panas <alexandra.panas@why.de>
2022-05-31 14:58:05 +0200

    chore: register multi media text element

--- lib/main.ts
+++ lib/main.ts
@@ -2,6 +2,7 @@ import { defineCustomElement } from './localApiCustomElement'
 
 import O9902Quote from '../src/components/organisms/o-99-02-quote.ce.vue'
 import O0304FullscreenMediaText from '../src/components/organisms/o-03-04-fullscreen-media-text.ce.vue'
+import O0312MultiMediaText from '../src/components/organisms/o-03-12-multi-media-text.ce.vue'
 
 const O9902QuoteElement = defineCustomElement(O9902Quote, { shadowRoot: false })
 const FullscreenMediaTextElement = defineCustomElement(
@@ -9,9 +10,14 @@ const FullscreenMediaTextElement = defineCustomElement(
   { shadowRoot: false }
 )
 
-export { O9902Quote, O0304FullscreenMediaText }
+const O0312MultiMediaTextElement = defineCustomElement(O0312MultiMediaText, {
+  shadowRoot: false
+})
+
+export { O9902Quote, O0304FullscreenMediaText, O0312MultiMediaText }
 
 export function register() {
   customElements.define('o-99-02-quote', O9902QuoteElement)
   customElements.define('fullscreen-media-text', FullscreenMediaTextElement)
+  customElements.define('o-03-12-multi-media-text', O0312MultiMediaTextElement)
 }
