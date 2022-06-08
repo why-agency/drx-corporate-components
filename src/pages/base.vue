04a1b9eefa1e18e2f318241af7e924caa88a6293
Jan Demmerle <jan.demmerle@why.de>
2022-06-08 13:27:42 +0200

    feat(14590614): implement checkbox component

--- src/pages/base.vue
+++ src/pages/base.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="my-10 mx-4">
-   <!-- <BaseFilter /> -->
+    <BaseCheckbox v-model="isChecked">Check me, please 🙌</BaseCheckbox>
+
+    <p class="mt-10">
+      Which animals do you like?
+      <span>{{ animalsThatILike.join(' ') }}</span>
+    </p>
+    <div class="flex gap-10 mt-6">
+      <BaseCheckbox v-model="animalsThatILike" value="🦊">🦊</BaseCheckbox>
+      <BaseCheckbox v-model="animalsThatILike" value="🦅">🦅</BaseCheckbox>
+      <BaseCheckbox v-model="animalsThatILike" value="🐍">🐍</BaseCheckbox>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-// import BaseFilter from '../components/base/Filter.vue'
+import { ref } from 'vue'
+
+import BaseCheckbox from '../components/base/Checkbox.vue'
+
+const isChecked = ref(false)
+const animalsThatILike = ref([])
 </script>
