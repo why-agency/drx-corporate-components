59c67618e698864250409d1147daa452b15e65d5
Alexandra Panas <alexandra.panas@why.de>
2022-06-09 18:33:13 +0200

    feat: add carousel + controls & buttons unstyled

---
+++ src/components/molecules/CarouselControls.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="flex frame-default space-x-2 absolute lg:bottom-20 left-0 invisible"
+  >
+    <BaseButtonIcon
+      :color="isDark ? 'white' : 'primary'"
+      variant="outline"
+      @click="go('<')"
+    >
+      <IconArrowLeftSmall />
+    </BaseButtonIcon>
+    <BaseButtonIcon
+      :color="isDark ? 'white' : 'primary'"
+      variant="outline"
+      @click="go('>')"
+    >
+      <IconArrowRightSmall />
+    </BaseButtonIcon>
+  </div>
+</template>
+
+<script setup>
+import BaseButtonIcon from '../base/ButtonIcon.vue'
+import IconArrowRightSmall from '../icons/Arrow/RightSmall.vue'
+import IconArrowLeft from '../icons/Arrow/LeftSmall.vue'
+
+const props = defineProps({
+  isDark: {
+    type: Boolean,
+    default: false
+  },
+  go: {
+    type: Function,
+    default: undefined
+  }
+})
+</script>
