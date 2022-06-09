7f1c49dce33f8c5e0ae886df8db402d1f8454ab6
Jan Demmerle <jan.demmerle@why.de>
2022-06-09 15:06:43 +0200

    feat(14590614): implement base chip component (#18)

--- src/pages/base.vue
+++ src/pages/base.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="my-10 mx-4">
+    <h2 class="text-h2 mb-6">Checkoxes ✅</h2>
+
     <BaseCheckbox v-model="isChecked">Check me, please 🙌</BaseCheckbox>
 
     <p class="mt-10">
@@ -11,6 +13,12 @@
       <BaseCheckbox v-model="animalsThatILike" value="🦅">🦅</BaseCheckbox>
       <BaseCheckbox v-model="animalsThatILike" value="🐍">🐍</BaseCheckbox>
     </div>
+
+    <h2 class="text-h2 mt-20">Chips 🍟</h2>
+    <div class="flex gap-4 mt-6">
+      <BaseChip @click="handleClick">Germany</BaseChip>
+      <BaseChip color="white" @click="handleClick">Clear all filters</BaseChip>
+    </div>
   </div>
 </template>
 
@@ -18,7 +26,10 @@
 import { ref } from 'vue'
 
 import BaseCheckbox from '../components/base/Checkbox.vue'
+import BaseChip from '../components/base/Chip.vue'
 
 const isChecked = ref(false)
+const handleClick = () => console.log('clicked')
+
 const animalsThatILike = ref([])
 </script>
