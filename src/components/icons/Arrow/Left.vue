09d78af68834751fb02767420bb16c03da8dbaaf
Alexandra Panas <alexandra.panas@why.de>
2022-06-09 18:32:06 +0200

    feat: add some icons

---
+++ src/components/icons/Arrow/Left.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    viewBox="0 0 24 24"
+    aria-labelledby="name"
+  >
+    <title id="IconArrowLeft" lang="en">IconArrowLeft</title>
+    <g fill="currentColor">
+      <path
+        d="M6.5 0.5L0 7L6.5 13.5L7.207 12.793L1.914 7.5H24V6.5H1.914L7.207 1.207L6.5 0.5Z"
+      />
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IconArrowLeft',
+  props: {
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    height: {
+      type: [Number, String],
+      default: 24
+    }
+  }
+}
+</script>
