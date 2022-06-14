74e06c3b17067589b23d6d1a3c826c437ce0827c
SandraKi <Sandra.Kindler@why.de>
2022-06-14 13:33:29 +0200

    feat: create kpi fullscreeen

---
+++ src/components/organisms/o-06-07-kpi-fullscreen/index.ce.vue
@@ -0,0 +1,51 @@
+<template>
+  <section>
+    <div :class="frame">
+      <div v-if="isMedia">
+        <BaseMedia
+          v-if="isMedia"
+          :media="media"
+          :video-settings="{ autoplay: true }"
+          :gradient="gradient.value"
+        />
+      </div>
+      <div v-else>
+        <BaseHeadline
+          v-bind="headline"
+          :class="{
+            'text-sand': background !== 'light',
+            'text-primary': background === 'light'
+          }"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { computed, onMounted, toRefs, ref } from 'vue'
+import BaseHeadline from '../../base/Headline.vue'
+import BaseMedia from '../../base/Media.vue'
+
+const props = defineProps({
+  data: {
+    type: Object,
+    default: () => ({})
+  }
+})
+
+const frameClass = ref(props.data?.appearance?.frameClass)
+const frame = 'frame-' + frameClass.value
+
+/** quote atoms */
+const {
+  header: headline,
+  background,
+  gradient,
+  media,
+  kpicards: cards
+} = toRefs(props.data.content)
+
+/** media set true */
+const isMedia = true /** muss noch weg!! */
+</script>
---
+++ src/pages/kpi-fullscreen.vue
@@ -0,0 +1,82 @@
+<template>
+  <div><o-06-07-kpi-fullscreen :data="kpi" /></div>
+</template>
+
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import O0607KpiFullscreen from '../components/organisms/o-06-07-kpi-fullscreen/index.ce.vue'
+</script>
+
+
+<script>
+export default {
+  components: { O0607KpiFullscreen },
+  data() {
+    return {
+      kpi: {
+        id: 78358,
+        pid: 9093,
+        type: 'o_06_07__kpi_fullscreen',
+        tagname: 'o-06-07-kpi-fullscreen',
+        appearance: {
+          layout: 'default',
+          frameClass: 'default',
+          spaceBefore: '',
+          spaceAfter: ''
+        },
+        content: {
+          background: 'dark',
+          gradient: 'light',
+          media: {
+            type: 'video-stream',
+            image: null,
+            video_stream: [
+              {
+                id: 2,
+                pid: 9093,
+                type: 'videos_stream',
+                properties: {
+                  videoservice: 'youtube',
+                  video_id: 'ScMzIvxBSi4&t=1s',
+                  video_title: 'This is a video title',
+                  video_poster_image: null,
+                  video_start: null,
+                  video_end: null,
+                  video_duration: ''
+                }
+              }
+            ]
+          },
+          header: { text: 'KPI + Quote + Image', tag: 'h2' },
+          kpicards: [
+            {
+              id: 1,
+              pid: 9093,
+              type: 'kpicard',
+              content: {
+                name: 'KPI 1',
+                value: '3939',
+                unit: 'kg',
+                description: 'sdsffsf'
+              }
+            },
+            {
+              id: 2,
+              pid: 9093,
+              type: 'kpicard',
+              content: {
+                name: 'KPI 2',
+                value: '3333',
+                unit: 'm2',
+                description: 'asdadd'
+              }
+            }
+          ],
+          overlays: []
+        }
+      }
+    }
+  }
+}
+</script>
