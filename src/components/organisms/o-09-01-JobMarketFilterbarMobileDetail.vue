75a70240c249cbad41a952d4fd3cf290eb9b8f75
Jan Demmerle <jan.demmerle@why.de>
2022-06-22 11:27:10 +0200

    feat(14590614): implement mobile filter bar

---
+++ src/components/organisms/o-09-01-JobMarketFilterbarMobileDetail.vue
@@ -0,0 +1,101 @@
+<template>
+  <div
+    class="fixed top-0 left-0 flex flex-col w-screen h-full min-h-screen px-6 py-8 bg-white z-50"
+  >
+    <button class="flex mb-12" @click="jobsStore.activeFilterView = null">
+      <IconChevronUp class="-rotate-90" />
+      <p class="ml-4 text-button font-bold text-primary uppercase">
+        Zurück zu den Filtern
+      </p>
+    </button>
+
+    <BaseHeadline :animate="false" :size="5">
+      {{ jobsStore.activeFilterView?.label }}
+    </BaseHeadline>
+
+    <!-- START filterable options -->
+    <div class="w-full flex-1 py-1 pl-1 mt-8 overflow-scroll grid grid-cols-1 gap-8 mask">
+      <BaseCheckbox
+        v-for="filter in relevantFilterOptions"
+        :key="filter.value"
+        v-model="filters"
+        :value="filter.value"
+        class="last:mb-16"
+      >
+        {{ filter.label }}
+      </BaseCheckbox>
+    </div>
+    <!-- END filterable options -->
+
+    <div class="flex flex-col justify-center pt-4">
+      <button class="text-button text-tertiary" @click="jobsStore.activeFilterView = null">
+        Zurück zu den Filtern
+      </button>
+      <BaseAction
+        class="mt-6 w-full"
+        variant="block"
+        block
+        @click="hideFilterBar"
+      >
+        {{ jobsStore.count }} Ergebnisse anzeigen
+      </BaseAction>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+// Hooks
+import { useScrollLock } from '../../composables/useScrollLock'
+import { useJobs } from '../../stores/jobs'
+
+// Components
+import IconChevronUp from '../icons/ChevronUp.vue'
+import BaseCheckbox from '../base/Checkbox.vue'
+import BaseAction from '../base/Action.vue'
+import BaseHeadline from '../base/Headline.vue'
+
+// Types
+import { FilterOption } from '../../../types'
+
+const jobsStore = useJobs()
+
+const relevantFilterOptions = computed(() =>
+  jobsStore.filterOptions.filter(
+    option => option.parent === jobsStore.activeFilterView?.name
+  )
+)
+
+const filters = computed({
+  get() {
+    return jobsStore.activeFilterOptions.map(
+      (filter: FilterOption) => filter.value
+    )
+  },
+  set(value: String[]) {
+    const activeFilterOptions = jobsStore.filterOptions.filter(option =>
+      value.includes(option.value)
+    )
+
+    jobsStore.setActiveFilterOptions(activeFilterOptions)
+  }
+})
+
+
+const isLocked = useScrollLock()
+const hideFilterBar = () => {
+  isLocked.value = false
+  jobsStore.isFilterBarActive = false
+  setTimeout(() => {
+    jobsStore.activeFilterView = null
+  }, 300)
+}
+</script>
+
+<style scoped>
+.mask {
+  -webkit-mask-image: linear-gradient(to bottom, black 80%, transparent 100%);
+  mask-image: linear-gradient(to bottom, black 80%, transparent 100%);
+}
+</style>
