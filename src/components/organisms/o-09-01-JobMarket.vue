faa44d2771fe8993eafc8a21c5be844d72fd6b1c
Jan Demmerle <jan.demmerle@why.de>
2022-06-22 18:16:43 +0200

    feat(14590614): display header image above job grid

--- src/components/organisms/o-09-01-JobMarket.vue
+++ src/components/organisms/o-09-01-JobMarket.vue
@@ -1,10 +1,15 @@
 <template>
   <section>
     <!-- START job market header -->
+    <div class="relative w-full h-96 after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-primary after:bg-opacity-60">
+      <BasePicture :images="data.image" size="w-full h-full" />
+
+      <BaseHeadline color="light" :text="data.header.text" class="absolute z-20 bottom-12 left-6" :size="3" />
+    </div>
     <!-- END job market header -->
 
     <!-- START job market grid -->
-    <div class="flex items-center justify-between">
+    <div class="flex items-center justify-between px-4 mt-12">
       <BaseTextField
         v-model="query"
         label="Keep looking for jobs"
@@ -16,16 +21,16 @@
           <IconSearch />
         </template>
       </BaseTextField>
-      <p class="text-body2 font-semibold font-primary">
+      <p class="text-body2 font-semibold font-primary ml-4">
         {{ jobsStore.count }} Jobs
       </p>
     </div>
 
-    <div v-if="jobsStore.isRequestPending" class="grid grid-cols-1 gap-4 mt-6">
+    <div v-if="jobsStore.isRequestPending" class="grid grid-cols-1 gap-4 mt-6 px-4">
       <o-09-01-JobMarketCardSkeleton v-for="card in 10" />
     </div>
 
-    <div v-else class="grid grid-cols-1 gap-4 mt-6">
+    <div v-else class="grid grid-cols-1 gap-4 mt-6 px-4">
       <o-09-01-JobMarketCard
         v-for="job in jobsStore.jobs.slice(0, 10)"
         v-bind="job"
@@ -53,6 +58,8 @@ import { useJobs } from '../../stores/jobs'
 import IconSearch from '../icons/Search.vue'
 
 import BaseTextField from '../base/TextField.vue'
+import BasePicture from '../base/Picture.vue'
+import BaseHeadline from '../base/Headline.vue'
 
 import O0901JobMarketCard from './o-09-01-JobMarketCard.vue'
 import O0901JobMarketCardSkeleton from './o-09-01-JobMarketCardSkeleton.vue'
