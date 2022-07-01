b52769a2c837ca9f4f3c5516d9c1ea145e0a79aa
Alexandra Panas <alexandra.panas@why.de>
2022-07-01 13:35:02 +0200

    fix: remove fixed height

--- src/components/organisms/o-06-09-media-hotspot/hotspot-detail.ce.vue
+++ src/components/organisms/o-06-09-media-hotspot/hotspot-detail.ce.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="px-6 pt-10 space-y-6 lg:px-0 lg:flex lg:space-y-0 lg:space-x-12 lg:pt-6 xl:pt-10 2xl:pt-16 pb-12 w-full h-content lg:h-52 bg-primary opacity-100 justify-center"
+    class="px-6 pt-10 space-y-6 lg:px-0 lg:flex lg:space-y-0 lg:space-x-12 lg:pt-6 xl:pt-10 2xl:pt-16 pb-12 w-full h-content bg-primary opacity-100 justify-center"
   >
     <BaseButtonIcon
       v-if="icon"
