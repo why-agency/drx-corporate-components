4014b63c58246ea9c7477d2c755842a47e3ebe51
Alexandra Panas <alexandra.panas@why.de>
2022-07-04 15:35:21 +0200

    feat: create overlay trigger

---
+++ src/components/organisms/o-06-08-overlay/trigger.vue
@@ -0,0 +1,27 @@
+<template>
+  <BaseAction color="tertiary" @click="showOverlay">
+    {{ triggerText }}
+    <template #icon>
+      <IconExpand />
+    </template>
+  </BaseAction>
+</template>
+
+<script setup>
+import BaseAction from '../../base/Action.vue'
+import IconExpand from '../../icons/Expand.vue'
+import { useUiStore } from '../../../stores/ui.ts'
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  overlay: {
+    type: Array,
+    default: null
+  }
+})
+
+const triggerText = 'Learn more'
+
+const ui = useUiStore()
+const showOverlay = () => ui.setOverlay(props.overlay)
+</script>
