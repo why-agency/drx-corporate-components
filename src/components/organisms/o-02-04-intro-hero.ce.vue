a846aaee0c9b91b9c187120b947fe20e1b02dfc8
SandraKi <Sandra.Kindler@why.de>
2022-07-05 16:23:56 +0200

    feat: add multiple versions

--- src/components/organisms/o-02-04-intro-hero.ce.vue
+++ src/components/organisms/o-02-04-intro-hero.ce.vue
@@ -3,32 +3,46 @@
     <div
       ref="triggerContainer"
       class="w-full lg:h-screen"
-      :class="$_backgroundColor"
+      :class="[$_backgroundColor, { 'h-content': !isDefaultVariant }]"
     >
-      <div v-if="media" ref="stickyImage" class="h-full w-full absolute">
+      <div
+        v-if="media"
+        ref="stickyImage"
+        class="h-full w-full absolute"
+        :class="{ 'relative lg:absolute': !isDefaultVariant }"
+      >
         <BaseMedia
           v-if="media"
           :media="media"
           :gradient="gradient"
           class="overflow-hidden"
-          :class="isDefaultVariant ? '!h-screen before:!h-screen' : '!h-[70%]'"
+          :class="
+            isDefaultVariant
+              ? '!h-screen before:!h-screen'
+              : 'h-[100vw] lg:!h-[70%]'
+          "
           full-screen
           :mediaStyle="
             isDefaultVariant
               ? 'h-screen w-[100vw] object-cover'
-              : '!h-full w-[100vw] object-cover'
+              : 'h-[100vw] lg:!h-full w-[100vw] object-cover'
           "
         />
       </div>
       <div
         class="flex flex-col justify-end frame-content-default relative z-30 pb-16 pt-[136px] lg:pt-[0px]"
-        :class="isDefaultVariant ? 'h-content lg:h-full' : 'h-[70%]'"
+        :class="
+          isDefaultVariant ? 'h-content lg:h-full' : 'h-[70%] hidden lg:flex'
+        "
       >
         <!-- If it is the default variant -->
         <div
           v-if="isDefaultVariant"
-          class="lg:mb-6 h-content lg:max-w-[40%] lg:h-[432px] flex flex-col"
-          :class="!subheadline && !bodytext.text ? 'mb-72' : 'mb-11'"
+          class="lg:mb-6 h-content lg:max-w-[45%] lg:h-[432px] flex flex-col"
+          :class="[
+            !subheadline && !bodytext.text ? 'mb-72' : 'mb-11',
+            { invisible: hideContent }
+          ]"
         >
           <div class="my-auto">
             <BaseHeadline
@@ -43,7 +57,7 @@
               tag="h4"
               :size="4"
               :color="$_textColor === 'text-primary' ? 'dark' : 'light'"
-              class="w-[70%]"
+              class="w-[60%]"
             />
             <!-- Bodytext -->
             <BaseText
@@ -60,13 +74,38 @@
           :actions="actions"
           :position="isLg ? 'right' : 'left'"
           variant="large"
+          :class="{ invisible: hideContent }"
         />
+        <BaseButtonIcon
+          v-if="media && isVideo"
+          variant="outline"
+          color="gray"
+          size="lg"
+          class="mt-6 lg:mt-0 lg:absolute"
+          @click="hideContent ? (hideContent = false) : (hideContent = true)"
+          :class="{ 'hidden lg:flex': !isDefaultVariant }"
+        >
+          <IconPlay v-if="!hideContent" class="w-5" />
+          <IconArrowLeft v-if="hideContent" class="w-5 pt-[15%]" />
+        </BaseButtonIcon>
       </div>
-      <div class="w-full">
+      <!-- If it is the headline_large version -->
+      <div class="w-full px-[5%] py-10 lg:p-0" v-if="!isDefaultVariant">
         <BaseHeadline
-          v-if="headline && headline.text && !isDefaultVariant"
+          v-if="headline && headline.text"
           v-bind="headline"
-          class="text-deco1 text-center !font-normal mt-[2%]"
+          class="text-deco1 !font-normal lg:mt-[2%] break-words text-left lg:text-center mb-16"
+        />
+        <!-- The mobile version of the headline_large version -->
+        <MActionBar
+          v-if="actions"
+          :actions="actions"
+          :position="isLg ? 'right' : 'left'"
+          variant="large"
+          :class="[
+            { 'lg:hidden': !isDefaultVariant },
+            { invisible: hideContent }
+          ]"
         />
       </div>
     </div>
@@ -79,6 +118,9 @@ import BaseMedia from '../base/Media.vue'
 import BaseHeadline from '../base/Headline.vue'
 import BaseText from '../base/Text.vue'
 import MActionBar from '../molecules/ActionBar.vue'
+import BaseButtonIcon from '../base/ButtonIcon.vue'
+import IconPlay from '../icons/Play.vue'
+import IconArrowLeft from '../icons/Arrow/Left.vue'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
@@ -113,8 +155,12 @@ const $_backgroundColor = computed(() => {
   return 'bg-' + background.value
 })
 
-const isDefaultVariant = computed(() => {
-  return variant.value === 'default'
+const isDefaultVariant = variant.value === 'default'
+
+const isVideo = computed(() => {
+  if (media.value) {
+    return media.value[0]?.[0]?.properties?.type === 'video'
+  }
 })
 
 const $_textColor = computed(() => {
@@ -128,16 +174,20 @@ const $_textColor = computed(() => {
     : 'text-sand'
 })
 
+const hideContent = ref(false)
+
 const triggerContainer = ref(null)
 const stickyImage = ref(null)
 onMounted(() => {
-  //scrollTrigger for the mobile sticky background
-  const scroll = ScrollTrigger.create({
-    trigger: triggerContainer.value,
-    start: 'top top',
-    end: 'bottom bottom',
-    pin: stickyImage.value,
-    onToggle: self => console.log('toggled, isActive:')
-  })
+  if (isDefaultVariant) {
+    //scrollTrigger for the mobile sticky background
+    const scroll = ScrollTrigger.create({
+      trigger: triggerContainer.value,
+      start: 'top top',
+      end: 'bottom bottom',
+      pin: stickyImage.value,
+      onToggle: self => console.log('toggled, isActive:')
+    })
+  }
 })
 </script>
--- src/pages/intro-hero.vue
+++ src/pages/intro-hero.vue
@@ -1,6 +1,15 @@
 <template>
   <section>
-    <o-02-04-intro-hero :data="hero" />
+    <o-02-04-intro-hero :data="hero0" class="mb-64" />
+  </section>
+  <section>
+    <o-02-04-intro-hero :data="hero1" class="mb-64" />
+  </section>
+  <section>
+    <o-02-04-intro-hero :data="hero2" class="mb-64"/>
+  </section>
+    <section>
+    <o-02-04-intro-hero :data="hero3" class="mb-64"/>
   </section>
 </template>
 
@@ -14,9 +23,9 @@ import O0204IntroHero from '../components/organisms/o-02-04-intro-hero.ce.vue'
 export default {
   data() {
     return {
-      hero: {
+      hero0: {
         content: {
-          variant: 'no',
+          variant: 'default',
           background: 'gradient',
           header: {
             text: 'Familienunternehmen seit 1958',
@@ -186,6 +195,226 @@ export default {
                 },
                 useCase: 'mobile',
                 ratio: null
+              }
+            ]
+          ],
+          gradient: 'dark',
+          actions: [
+            {
+              id: 7,
+              pid: 9088,
+              type: 'a_01_01__action',
+              content: {
+                text: 'test',
+                link: {
+                  type: 'page',
+                  url: '\/karriere',
+                  target: ' target="_top"',
+                  title: '',
+                  class: ''
+                },
+                icon: null,
+                color: 'secondary'
+              }
+            },
+            {
+              id: 8,
+              pid: 9088,
+              type: 'a_01_01__action',
+              content: {
+                text: 'button',
+                link: {
+                  type: 'page',
+                  url: '\/karriere',
+                  target: ' target="_top"',
+                  title: '',
+                  class: ''
+                },
+                icon: null,
+                color: 'secondary'
+              }
+            },
+            {
+              id: 9,
+              pid: 9088,
+              type: 'a_01_01__action',
+              content: {
+                text: 'Nachhaltigkeit',
+                link: {
+                  type: 'page',
+                  url: '\/karriere',
+                  target: ' target="_top"',
+                  title: '',
+                  class: ''
+                },
+                icon: null,
+                color: 'white'
+              }
+            }
+          ]
+        }
+      },
+      hero1: {
+        content: {
+          variant: 'default',
+          background: 'gradient',
+          header: {
+            text: 'Familienunternehmen seit 1958',
+            layout: '1',
+            color: 'light',
+            tag: 'h1'
+          },
+          subheadline: '',
+          bodytext: {
+            text: 'Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdie.',
+            layout: '1'
+          },
+          media: null,
+          gradient: 'dark',
+          actions: [
+            {
+              id: 7,
+              pid: 9088,
+              type: 'a_01_01__action',
+              content: {
+                text: 'test',
+                link: {
+                  type: 'page',
+                  url: '\/karriere',
+                  target: ' target="_top"',
+                  title: '',
+                  class: ''
+                },
+                icon: null,
+                color: 'secondary'
+              }
+            }
+          ]
+        }
+      },
+      hero2: {
+        content: {
+          variant: 'headline_large',
+          background: 'gradient',
+          header: {
+            text: 'Familienunternehmen seit 1958',
+            layout: '1',
+            color: 'light',
+            tag: 'h1'
+          },
+          subheadline: 'Der Vorstand der DRÄXLMAIER Group',
+          bodytext: {
+            text: 'Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdie.',
+            layout: '1'
+          },
+          media: [
+            [
+              {
+                publicUrl:
+                  'https:\/\/development.draexlmaier.com\/fileadmin\/user_upload\/Neues_Video.mp4',
+                properties: {
+                  uidLocal: 63453,
+                  type: 'video',
+                  ext: 'mp4',
+                  identifier: '\/user_upload\/Neues_Video.mp4',
+                  url: 'fileadmin\/user_upload\/Neues_Video.mp4'
+                },
+                cdn: {
+                  publicUrl: 'fileadmin\/user_upload\/Neues_Video.mp4',
+                  publicPath: '\/user_upload\/Neues_Video.mp4',
+                  note: 'For this mime type CDN is not relevant!'
+                }
+              }
+            ]
+          ],
+          gradient: 'dark',
+          actions: [
+            {
+              id: 7,
+              pid: 9088,
+              type: 'a_01_01__action',
+              content: {
+                text: 'test',
+                link: {
+                  type: 'page',
+                  url: '\/karriere',
+                  target: ' target="_top"',
+                  title: '',
+                  class: ''
+                },
+                icon: null,
+                color: 'secondary'
+              }
+            },
+            {
+              id: 8,
+              pid: 9088,
+              type: 'a_01_01__action',
+              content: {
+                text: 'button',
+                link: {
+                  type: 'page',
+                  url: '\/karriere',
+                  target: ' target="_top"',
+                  title: '',
+                  class: ''
+                },
+                icon: null,
+                color: 'secondary'
+              }
+            },
+            {
+              id: 9,
+              pid: 9088,
+              type: 'a_01_01__action',
+              content: {
+                text: 'Nachhaltigkeit',
+                link: {
+                  type: 'page',
+                  url: '\/karriere',
+                  target: ' target="_top"',
+                  title: '',
+                  class: ''
+                },
+                icon: null,
+                color: 'white'
+              }
+            }
+          ]
+        }
+      },
+      hero3: {
+        content: {
+          variant: 'default',
+          background: 'gradient',
+          header: {
+            text: 'Familienunternehmen seit 1958',
+            layout: '1',
+            color: 'light',
+            tag: 'h1'
+          },
+          subheadline: 'Der Vorstand der DRÄXLMAIER Group',
+          bodytext: {
+            text: 'Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdie.',
+            layout: '1'
+          },
+          media: [
+            [
+              {
+                publicUrl:
+                  'https:\/\/development.draexlmaier.com\/fileadmin\/user_upload\/Neues_Video.mp4',
+                properties: {
+                  uidLocal: 63453,
+                  type: 'video',
+                  ext: 'mp4',
+                  identifier: '\/user_upload\/Neues_Video.mp4',
+                  url: 'fileadmin\/user_upload\/Neues_Video.mp4'
+                },
+                cdn: {
+                  publicUrl: 'fileadmin\/user_upload\/Neues_Video.mp4',
+                  publicPath: '\/user_upload\/Neues_Video.mp4',
+                  note: 'For this mime type CDN is not relevant!'
+                }
               }
             ]
           ],
