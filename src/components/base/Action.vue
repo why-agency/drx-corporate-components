c4aa46b241be78193589ca4fba69ed19f44e1db0
Alexandra Panas <alexandra.panas@why.de>
2022-07-06 14:48:35 +0200

    fix: prevent button text from wrapping

--- src/components/base/Action.vue
+++ src/components/base/Action.vue
@@ -104,7 +104,7 @@ export default {
     },
     $_size() {
       return {
-        'min-h-[48px] min-w-[224px] max-w-fit': this.variant === 'default',
+        'min-h-[48px] min-w-[224px] max-w-max': this.variant === 'default',
         'min-h-[48px] w-full': this.variant === 'block',
         'w-[328px] h-[88px] lg:w-[232px] lg:h-[112px]': this.variant === 'large'
       }
