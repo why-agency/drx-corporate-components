984643a6627d6f200df84080de5b596ebedf0600
SandraKi <Sandra.Kindler@why.de>
2022-07-07 15:34:59 +0200

    fix: revise intro hero

--- src/components/organisms/o-02-04-intro-hero.ce.vue
+++ src/components/organisms/o-02-04-intro-hero.ce.vue
@@ -1,113 +1,110 @@
 <template>
-  <section>
+  <section
+    ref="triggerContainer"
+    class="w-full lg:h-screen"
+    :class="{ 'h-max': !isDefaultVariant }"
+  >
     <div
-      ref="triggerContainer"
-      class="w-full lg:h-screen"
-      :class="[$_backgroundColor, { 'h-content': !isDefaultVariant }]"
+      v-if="media"
+      ref="stickyImage"
+      class="h-full w-full absolute"
+      :class="{ 'relative lg:absolute': !isDefaultVariant }"
     >
-      <div
-        v-if="media"
-        ref="stickyImage"
-        class="h-full w-full absolute"
-        :class="{ 'relative lg:absolute': !isDefaultVariant }"
-      >
-        <BaseMedia
-          v-if="media"
-          :media="media"
-          :gradient="gradient"
-          class="overflow-hidden"
-          :class="
-            isDefaultVariant
-              ? '!h-screen before:!h-screen'
-              : 'h-[100vw] lg:!h-[70%]'
-          "
-          full-screen
-          :mediaStyle="
-            isDefaultVariant
-              ? 'h-screen w-[100vw] object-cover'
-              : 'h-[100vw] lg:!h-full w-[100vw] object-cover'
-          "
-        />
-      </div>
-      <div
-        class="flex flex-col justify-end frame-content-default relative z-30 pb-16 pt-[136px] lg:pt-[0px]"
+      <BaseMedia
+        :media="media"
+        :gradient="gradient"
+        class="overflow-hidden"
         :class="
-          isDefaultVariant ? 'h-content lg:h-full' : 'h-[70%] hidden lg:flex'
+          isDefaultVariant
+            ? '!h-screen before:!h-screen'
+            : 'h-[100vw] lg:!h-[70%]'
         "
+        full-screen
+        :mediaStyle="
+          isDefaultVariant
+            ? 'h-screen w-[100vw] object-cover'
+            : 'h-[100vw] lg:!h-full w-[100vw] object-cover'
+        "
+      />
+    </div>
+    <div
+      class="flex flex-col justify-end frame-content-default relative z-30 pb-16 pt-[136px] lg:pt-[0px]"
+      :class="
+        isDefaultVariant ? 'h-max lg:h-full' : 'h-[70%] hidden lg:flex'
+      "
+    >
+      <!-- If it is the default variant -->
+      <div
+        v-if="isDefaultVariant"
+        class="lg:mb-6 h-max lg:max-w-[45%] lg:h-[432px] flex flex-col"
+        :class="[
+          !subheadline && !bodytext.text ? 'mb-72' : 'mb-11',
+          { invisible: hideContent }
+        ]"
       >
-        <!-- If it is the default variant -->
-        <div
-          v-if="isDefaultVariant"
-          class="lg:mb-6 h-content lg:max-w-[45%] lg:h-[432px] flex flex-col"
-          :class="[
-            !subheadline && !bodytext.text ? 'mb-72' : 'mb-11',
-            { invisible: hideContent }
-          ]"
-        >
-          <div class="my-auto">
-            <BaseHeadline
-              v-if="headline && headline.text"
-              v-bind="headline"
-              class="!font-normal pb-7 lg:pb-12 break-words"
-            />
-            <!-- Subline -->
-            <BaseHeadline
-              v-if="subheadline"
-              :text="subheadline"
-              tag="h4"
-              :size="4"
-              :color="$_textColor === 'text-primary' ? 'dark' : 'light'"
-              class="w-[60%]"
-            />
-            <!-- Bodytext -->
-            <BaseText
-              v-if="bodytext && bodytext.text"
-              :text="bodytext.text"
-              :size="$_bodytextSize"
-              :class="$_textColor"
-              class="leading-7 pt-24 lg:pt-5"
-            />
-          </div>
+        <div class="my-auto">
+          <BaseHeadline
+            v-if="headline && headline.text"
+            v-bind="headline"
+            class="!font-normal pb-7 lg:pb-12 break-words"
+          />
+          <!-- Subline -->
+          <BaseHeadline
+            v-if="subheadline"
+            :text="subheadline"
+            tag="h4"
+            :size="4"
+            :color="$_textColor === 'text-primary' ? 'dark' : 'light'"
+            class="w-[60%]"
+          />
+          <!-- Bodytext -->
+          <BaseText
+            v-if="bodytext && bodytext.text"
+            :text="bodytext.text"
+            :size="$_bodytextSize"
+            :class="$_textColor"
+            class="leading-7 pt-24 lg:pt-5"
+          />
         </div>
-        <MActionBar
-          v-if="actions"
-          :actions="actions"
-          :position="isLg ? 'right' : 'left'"
-          variant="large"
-          :class="{ invisible: hideContent }"
-        />
-        <BaseButtonIcon
-          v-if="media && isVideo"
-          variant="outline"
-          color="gray"
-          size="lg"
-          class="mt-6 lg:mt-0 lg:absolute"
-          @click="hideContent ? (hideContent = false) : (hideContent = true)"
-          :class="{ 'hidden lg:flex': !isDefaultVariant }"
-        >
-          <IconPlay v-if="!hideContent" class="w-5" />
-          <IconArrowLeft v-if="hideContent" class="w-5 pt-[15%]" />
-        </BaseButtonIcon>
       </div>
-      <!-- If it is the headline_large version -->
-      <div class="w-full px-[5%] py-10 lg:p-0" v-if="!isDefaultVariant">
-        <BaseHeadline
-          v-if="headline && headline.text"
-          v-bind="headline"
-          class="text-deco1 !font-normal lg:mt-[2%] break-words text-left lg:text-center mb-16"
-        />
-        <!-- The mobile version of the headline_large version -->
-        <MActionBar
-          v-if="actions"
-          :actions="actions"
-          :position="isLg ? 'right' : 'left'"
-          variant="large"
-          :class="[
-            { 'lg:hidden': !isDefaultVariant },
-            { invisible: hideContent }
-          ]"
-        />
-      </div>
+      <MActionBar
+        v-if="actions"
+        :actions="actions"
+        :position="isLg ? 'right' : 'left'"
+        variant="large"
+        :class="{ invisible: hideContent }"
+      />
+      <BaseButtonIcon
+        v-if="media && isVideo"
+        variant="outline"
+        color="gray"
+        size="lg"
+        class="!mt-6 lg:mt-0 lg:absolute"
+        @click="hideContent = !hideContent"
+        :class="{ 'hidden lg:flex': !isDefaultVariant }"
+      >
+        <IconPlay v-if="!hideContent" class="w-5" />
+        <IconArrowLeft v-if="hideContent" class="w-5 pt-[15%]" />
+      </BaseButtonIcon>
+    </div>
+    <!-- If it is the headline_large version -->
+    <div class="w-full px-[5%] py-10 lg:p-0" v-if="!isDefaultVariant">
+      <BaseHeadline
+        v-if="headline && headline.text"
+        v-bind="headline"
+        class="text-deco1 !font-normal lg:mt-[2%] break-words text-left lg:text-center mb-16"
+      />
+      <!-- The mobile version of the headline_large version -->
+      <MActionBar
+        v-if="actions"
+        :actions="actions"
+        :position="isLg ? 'right' : 'left'"
+        variant="large"
+        :class="[
+          { 'lg:hidden': !isDefaultVariant },
+          { invisible: hideContent }
+        ]"
+      />
     </div>
   </section>
 </template>
@@ -131,7 +128,6 @@ const props = defineProps({
   }
 })
 
-// ÜBERARBEITEN
 const {
   variant,
   background,
@@ -147,29 +143,23 @@ const breakpoints = useBreakpoints(breakpointsTailwind)
 const isLg = breakpoints.greater('lg')
 gsap.registerPlugin(ScrollTrigger)
 
-const $_bodytextSize = computed(() => {
-  return 'text-body' + bodytext.value.layout
-})
-
-const $_backgroundColor = computed(() => {
-  return 'bg-' + background.value
-})
+const $_bodytextSize = computed(() => ({
+  'text-body1': bodytext.value.layout === '1',
+  'text-body2': bodytext.value.layout === '2',
+  'text-body3': bodytext.value.layout === '3'
+}))
 
 const isDefaultVariant = variant.value === 'default'
 
-const isVideo = computed(() => {
-  if (media.value) {
-    return media.value[0]?.[0]?.properties?.type === 'video'
-  }
-})
+const isVideo = computed(
+  () => media.value?.[0]?.[0]?.properties?.type === 'video'
+)
 
 const $_textColor = computed(() => {
   if (media.value) {
     return gradient.value === 'light' ? 'text-primary' : 'text-sand'
   }
-  return background.value === 'light'
-    ? 'text-primary'
-    : background.value === 'none'
+  return background.value === 'light' || background.value === 'none'
     ? 'text-primary'
     : 'text-sand'
 })
@@ -179,14 +169,13 @@ const hideContent = ref(false)
 const triggerContainer = ref(null)
 const stickyImage = ref(null)
 onMounted(() => {
-  if (isDefaultVariant) {
+  if (isDefaultVariant && stickyImage.value) {
     //scrollTrigger for the mobile sticky background
     const scroll = ScrollTrigger.create({
       trigger: triggerContainer.value,
       start: 'top top',
       end: 'bottom bottom',
-      pin: stickyImage.value,
-      onToggle: self => console.log('toggled, isActive:')
+      pin: stickyImage.value
     })
   }
 })
