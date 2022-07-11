01f4d8c7cd3f6f0f0133f13c4f7b86204fc78365
alexandrapanas <83907451+alexandrapanas@users.noreply.github.com>
2022-07-11 14:36:49 +0200

    refactor: remove tooltip from arrow icons (#93)

--- src/components/icons/Arrow/RightSmall.vue
+++ src/components/icons/Arrow/RightSmall.vue
@@ -6,7 +6,6 @@
     viewBox="0 0 24 24"
     aria-labelledby="name"
   >
-    <title id="IconArrowRightSmall" lang="en">IconArrowRightSmall</title>
     <g fill="currentColor">
       <path d="M12.3 23.4L24 11.7L12.3 -6.99759e-06L11.0274 1.27259L20.5548 10.8L-3.14712e-06 10.8V12.6L20.5548 12.6L11.0274 22.1274L12.3 23.4Z"/>
     </g>
