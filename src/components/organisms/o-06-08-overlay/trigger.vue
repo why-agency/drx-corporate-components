995495873a9a5f4d76d9b8e0d75c8a440c0b5948
alexandrapanas <83907451+alexandrapanas@users.noreply.github.com>
2022-07-13 21:09:03 +0200

    feat: include overlay trigger in kpi reveal (#107)

--- src/components/organisms/o-06-08-overlay/trigger.vue
+++ src/components/organisms/o-06-08-overlay/trigger.vue
@@ -1,7 +1,9 @@
 <template>
   <BaseAction
+    ref="target"
     color="tertiary"
     class="text-left"
+    :class="{ 'translate-y-4 opacity-0': animate }"
     @click="$emit('show-overlay')"
   >
     {{ triggerText }}
@@ -12,16 +14,40 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, ref, watch } from 'vue'
 import BaseAction from '../../base/Action.vue'
 import IconExpand from '../../icons/Expand.vue'
+import { useIntersectionObserver } from '../../../composables/useIntersectionObserver'
+import { gsap, Power2 } from 'gsap'
 
 const props = defineProps({
   overlay: {
     type: Array,
     default: null
+  },
+  animate: {
+    type: Boolean,
+    default: false
   }
 })
 
-const triggerText = computed(() => props.overlay?.[0]?.content?.buttonText || 'Mehr erfahren')
+const triggerText = computed(
+  () => props.overlay?.[0]?.content?.buttonText || 'Mehr erfahren'
+)
+
+//overlay trigger reveal animation
+const target = ref(null)
+const isVisible = useIntersectionObserver({
+  target
+})
+watch(isVisible, isVisible => {
+  if (isVisible && props.animate && target.value?.$el) {
+    gsap.to(target.value?.$el, {
+      y: 0,
+      opacity: 1,
+      duration: 1.5,
+      ease: Power2.easeOut
+    })
+  }
+})
 </script>
