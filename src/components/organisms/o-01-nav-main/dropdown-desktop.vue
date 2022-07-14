6f5fc2c4c4bf3eb60684190afb90f9995e786cac
Alexandra Panas <alexandra.panas@why.de>
2022-07-14 12:20:03 +0200

    refactor: create component for dropdown column

--- src/components/organisms/o-01-nav-main/dropdown-desktop.vue
+++ src/components/organisms/o-01-nav-main/dropdown-desktop.vue
@@ -14,43 +14,15 @@
           class="text-secondary hover:text-tertiary flex items-end"
         >
           {{ data.navDropdownHeadline }}
-          <IconArrowRightFatShort class ="ml-2 h-6" />
+          <IconArrowRightFatShort class="ml-2 h-6" />
         </BaseHeadline>
       </UseDynamicAction>
       <div class="flex mt-12 justify-between">
         <div class="flex">
-          <div
-            v-if="firstChildren.length !== 0"
-            class="flex flex-col space-y-7 w-[312px] h-[258px] border-r border-tertiary border-opacity-50"
-          >
-            <!-- Left side -->
-            <UseDynamicAction
-              v-for="child in firstChildren"
-              :key="child"
-              :to="child.link"
-              :tag="child.link ? 'a' : 'div'"
-              :target="child.target"
-              class="hover:text-tertiary"
-            >
-              <BaseText :text="child.title" class="text-body2" />
-            </UseDynamicAction>
-          </div>
-          <div
-            v-if="lastChildren.length !== 0"
-            class="flex flex-col space-y-7 pl-16 w-[312px] h-[258px] border-r border-tertiary border-opacity-50"
-          >
-            <!-- Right side -->
-            <UseDynamicAction
-              v-for="child in lastChildren"
-              :key="child"
-              :to="child.link"
-              :tag="child.link ? 'a' : 'div'"
-              :target="child.target"
-              class="hover:text-tertiary"
-            >
-              <BaseText :text="child.title" class="text-body2" />
-            </UseDynamicAction>
-          </div>
+          <!-- Left side -->
+          <dropdown-column v-if="firstChildren.length" :items="firstChildren" />
+          <!-- Right side -->
+          <dropdown-column v-if="lastChildren.length" :items="lastChildren" class="pl-16" />
         </div>
         <div
           class="flex flex-col justify-between min-w-[388px] max-w-[508px] h-[258px] ml-16 text-body2"
@@ -120,6 +92,8 @@ import BaseHtmlParser from '../../base/HtmlParser.vue'
 import UseDynamicAction from '../../organisms/UseDynamicAction.vue'
 import IconArrowRightFatShort from '../../icons/Arrow/RightFatShort.vue'
 import IconArrowRight from '../../icons/Arrow/Right.vue'
+import DropdownColumn from './dropdown-column.vue'
+
 import { computed } from 'vue'
 const props = defineProps({
   data: {
