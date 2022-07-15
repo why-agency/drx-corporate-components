99af04ad0cb77f9706adde25c5fd4e6d604d1584
alexandrapanas <83907451+alexandrapanas@users.noreply.github.com>
2022-07-15 18:32:56 +0200

    Feat/search form (#115)
    * feat: create search form
    
    * fix: search form action
    
    * feat: use scroll lock if overlay is visible
    
    * chore: increase components version

--- src/components/organisms/o-01-nav-main/index.ce.vue
+++ src/components/organisms/o-01-nav-main/index.ce.vue
@@ -123,7 +123,7 @@ import {
   useBreakpoints,
   breakpointsTailwind
 } from '@vueuse/core'
-import { ref, computed, toRefs, onMounted, onBeforeUnmount } from 'vue'
+import { ref, computed, toRefs, onMounted, onBeforeUnmount, watch } from 'vue'
 import MLanguageSwitch from '../../molecules/LanguageSwitch.vue'
 import ButtonIcon from '../../base/ButtonIcon.vue'
 import BaseLogo from '../../base/Logo.vue'
@@ -140,6 +140,7 @@ import SearchOverlay from './search-overlay.vue'
 
 import { useNav } from '../../../stores/nav'
 import { useSearch } from '../../../stores/search'
+import { useScrollLock } from '../../../composables/useScrollLock.ts'
 
 const props = defineProps({
   data: {
@@ -261,4 +262,11 @@ const unsubscribe = searchStore.$onAction(({ name, after }) => {
   })
 })
 onBeforeUnmount(() => unsubscribe())
+
+// scroll lock
+const isLocked = useScrollLock()
+watch(
+  isOverlayVisible,
+  isOverlayVisible => (isLocked.value = !!isOverlayVisible)
+)
 </script>
