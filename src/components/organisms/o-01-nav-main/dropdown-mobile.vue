9afda324e8a94661c2efc9c418cff9605d2610b6
Alexandra Panas <alexandra.panas@why.de>
2022-07-15 19:48:43 +0200

    fix: remove unused imports and override old styles in dropdown mobile

--- src/components/organisms/o-01-nav-main/dropdown-mobile.vue
+++ src/components/organisms/o-01-nav-main/dropdown-mobile.vue
@@ -12,7 +12,7 @@
         v-if="data.additionalNaviHeader1 || data.additionalNaviText1"
         :header="data.additionalNaviHeader1"
         :text="data.additionalNaviText1"
-        class="mb-8"
+        class="!mb-8"
       />
       <additional-text
         v-if="data.additionalNaviHeader2 || data.additionalNaviHeader2"
@@ -25,10 +25,6 @@
 </template>
 
 <script setup>
-import BaseHeadline from '../../base/Headline.vue'
-import BaseHtmlParser from '../../base/HtmlParser.vue'
-import UseDynamicAction from '../../organisms/UseDynamicAction.vue'
-import IconArrowRightFatShort from '../../icons/Arrow/RightFatShort.vue'
 import DropdownColumn from './dropdown-column.vue'
 import AdditionalText from './additional-text.vue'
 
