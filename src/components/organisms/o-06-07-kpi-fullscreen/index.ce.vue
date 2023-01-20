0cf016def9309dac31e95798a61e0cbc95cecea9
SandraKi <Sandra.Kindler@why.de>
2023-01-20 19:03:46 +0100

    Fix/14964996 kpi fullscreen space after text (#154)
    * fix: h-max if no cards and no mediabackground
    
    * fix: restore kpi fullscreen testing page
    
    * fix: restore kpi fullscreen testing page
    
    * fix: kpi-fullscreen-space
    
    * fix: add console log for control
    
    * fix: if image is empty string

--- src/components/organisms/o-06-07-kpi-fullscreen/index.ce.vue
+++ src/components/organisms/o-06-07-kpi-fullscreen/index.ce.vue
@@ -120,7 +120,7 @@ const {
 } = toRefs(props.data.content)
 
 const isMedia = computed(() => {
-  return !media.value?.image || media.value?.video_stream?.length !== 0
+  return media.value?.image !== '' || media.value?.video_stream?.length !== 0
 })
 
 const isVideo = computed(() => {
@@ -159,6 +159,9 @@ const content = ref(null)
 const contentNoOverflow = ref(false)
 
 onMounted(() => {
+  console.log(cards.value)
+  console.log(isMedia.value)
+  console.log(hasQuoteContent.value)
   contentNoOverflow.value = content.value.clientHeight < window.innerHeight
   //scrollTrigger for the mobile sticky background
   const scroll = ScrollTrigger.create({
