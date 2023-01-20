93cd0b1b4a8d959e292b9a6f7ef3fdb97396142d
SandraKi <Sandra.Kindler@why.de>
2023-01-20 17:59:08 +0100

    fix: add console log for control

--- src/components/organisms/o-06-07-kpi-fullscreen/index.ce.vue
+++ src/components/organisms/o-06-07-kpi-fullscreen/index.ce.vue
@@ -159,6 +159,9 @@ const content = ref(null)
 const contentNoOverflow = ref(false)
 
 onMounted(() => {
+  console.log(cards.value)
+  console.log(isMedia.value)
+  console.log(hasQuoteContent.value)
   contentNoOverflow.value = content.value.clientHeight < window.innerHeight
   //scrollTrigger for the mobile sticky background
   const scroll = ScrollTrigger.create({
